b3f9c6a1901ea0eac0c987ca241fb28a3331f266
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-06-30 10:49:02 +0200

    wrote function prepareString

--- formatting.js
+++ formatting.js
@@ -1,3 +1,11 @@
+const prepareString = input => {
+  const withoutSpaces = removeWhitespace(input);
+  const stringArray = withoutSpaces.split(" ");
+  const resultArray = stringArray.map(string => capitalizeInitial(string));
+  const result = resultArray.join("_");
+  return result;
+}
+
 const removeWhitespace = input => {
   const trimmed = input.trim();
   const result = trimmed.replace(/\s+/," ");
@@ -8,4 +16,4 @@ const capitalizeInitial = input => {
   return `${input.charAt(0).toUpperCase()}${input.slice(1).toLowerCase()}`;
 }
 
-console.log(capitalizeInitial('kuala  lumpur    '));
+console.log(prepareString('  kuala  lumpur    '));
